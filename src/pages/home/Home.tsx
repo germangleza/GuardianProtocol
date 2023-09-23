f01ee891e022953c40ef8e8ce5c537aa01555f8e
Danyiel Colin <danyiel5978@gmail.com>
2023-09-22 19:36:44 -0700

    added Input functions to frontend

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,6 +8,78 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel,Table,
   TableContainer,Button, background,extendTheme} from '@chakra-ui/react'
 import { assets } from '@polkadot/types/interfaces/definitions';
 
+import { Input } from "@gear-js/ui";
+import { useState } from "react";
+
+import { GearApi } from "@gear-js/api";
+import { Button, Input } from "@gear-js/ui";
+import { useState } from "react";
+
+function GetAllEvents() {
+  const [allevents, setAllevents] = useState<any | undefined>(0);
+  const [blockhash, setBlockhash] = useState<any | undefined>("");
+
+  const subscribeblocks = async () => {
+    const gearApi = await GearApi.create({
+      providerAddress: "wss://testnet.vara.rs",
+    });
+
+    const unsub = await gearApi.gearEvents.subscribeToNewBlocks(
+      (header: any) => {
+        console.log(
+          `New block with number: ${header.number.toNumber()} and hash: ${header.hash.toHex()}`
+        );
+      }
+    );
+  };
+
+  const getallevents = async () => {
+    const gearApi = await GearApi.create({
+      providerAddress: "wss://rpc-node.gear-tech.io",
+    });
+
+    const events = await gearApi.blocks.getEvents(blockhash);
+    events.forEach((event) => {
+      setAllevents(event.toHuman());
+      console.log("Events", event.toHuman());
+    });
+  };
+
+  const AmountInputChange = async (event: any) => {
+    setBlockhash(event.target.value);
+  };
+
+  return (
+    <div className="card">
+      <center>Get All Events with Hash</center>
+      <Input type="text" value={blockhash} onChange={AmountInputChange} />
+      <div className="horizontal">
+        <Button text="Subscribe blocks" onClick={subscribeblocks} />
+        <Button text="Get All Events" onClick={getallevents} />
+      </div>
+    </div>
+  );
+}
+
+export { GetAllEvents }
+
+function InputComponent() {
+ 
+  const [text, setText] = useState<any | undefined>("");
+
+  const AmountInputChange = async (event: any) => {
+    setBlockhash(event.target.value);
+  };
+
+  return (
+    
+      <Input type="text" value={text} onChange={AmountInputChange} />
+     
+  );
+}
+
+export { InputComponent };
+
 function Home() {
 
   return (
@@ -48,8 +120,6 @@ function Home() {
         <Td id='end' ><Button position='inherit' marginBottom='7px' >MAX</Button></Td>
       </Tr>
       
-    
-    
       <Tr textColor='white'>
         <Td fontWeight='bold' >You will recieve</Td>
         <Td style={{ visibility: 'hidden' }}>.</Td>
